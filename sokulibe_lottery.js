2543c6ae2e1a580fe4068d78f7d8e42db901b35d
catchtest <catchtest@gmail.com>
2017-04-06 14:40:23 +0800

    refactor lottery

--- sokulibe_lottery.js
+++ sokulibe_lottery.js
@@ -47,9 +47,23 @@
 	}
 	
 	this.change = function(id) {
-		console.log(id);
-		console.log(define[id].data);
 		lotteryData = define[id].data;
+		
+		if (defineObj[id].basic !== false) {
+			// 增加二、三星的角色
+			lotteryData.push({
+                "rarity": 3,
+                "rate": 3000,
+                "unit_id": [29, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70],
+                "festival": false
+            });
+			lotteryData.push({
+                "rarity": 2,
+                "rate": 6400,
+                "unit_id": [21, 22, 23, 24, 25, 26, 27, 28, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40],
+                "festival": false
+            });
+		}
 		this.reset();
 	}
 	
